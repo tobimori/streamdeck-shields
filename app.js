1ce8d479b5c9c5f03d31626f2addb154ed7e9994
tobimori <tobias@moeritz.cc>
2020-05-12 18:42:15 +0200

    🎉 initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import axios from 'axios';
+import { setupCache } from 'axios-cache-adapter'
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+const cache = setupCache({
+  maxAge: 21600
+})
+
+const api = axios.create({
+  adapter: cache.adapter
+})
+
+app.get('/', (req, res) => {
+  res.status(200).send("Welcome to StreamDeck Shields.io Custom Endpoint by tobimori 👋") 
+});
+app.get('/shields/downloads/:id', (req, res) => {
+  console.log(`[${new Date()}] Received /shields/downloads request for ${req.params.id}`);
+  api({'url': 'https://appstore.elgato.com/streamDeckPlugin/catalog.json', method: 'get'})
+    .then(async (response) => {
+      let downloads = "error whilst retrieving data";
+      for (let e of response.data.plugins) {
+        if (e.identifier === req.params.id) downloads = `${e.downloads}`
+      };
+      res.status(200).send({
+        "schemaVersion": 1,
+        "label": "downloads",
+        "message": downloads,
+        "color": downloads === "error whilst retrieving data" && "critical" || "blue",
+        "style": "flat", 
+        "isError": downloads === "error whilst retrieving data",
+      })
+    })
+    .catch(function (error) {
+      res.status(500).send({
+        "error": error
+      })
+      console.log(error);
+    })
+}); 
+
+app.listen(process.env.PORT, () => {
+  console.log(`[${new Date()}] Server started on port ${process.env.PORT}`)
+});
